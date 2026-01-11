94227a0dc8ecafd958c227e59a4ecd65d98ba0a9
Rcvls <254138130+Rcvls@users.noreply.github.com>
2026-01-11 10:59:59 +0100

    More ideologies

--- ideologies.js
+++ ideologies.js
@@ -4,7 +4,7 @@ ideologies = [
         "stats": {
             "bigt": 80,
             "diff": 100,
-            "asim": 30,
+            "asim": 20,
             "plur": 0,
             "plzn": 100,
             "part": 100
@@ -13,12 +13,23 @@ ideologies = [
     {
         "name": "Liberal Eugenicist",
         "stats": {
-            "bigt": 20,
+            "bigt": 15,
             "diff": 80,
             "asim": 30,
-            "plur": 30,
-            "plzn": 60,
+            "plur": 40,
+            "plzn": 70,
             "part": 30
+        }
+    },
+    {
+        "name": "Eugenicist",
+        "stats": {
+            "bigt": 25,
+            "diff": 90,
+            "asim": 20,
+            "plur": 30,
+            "plzn": 80,
+            "part": 40
         }
     },
     {
@@ -28,8 +39,118 @@ ideologies = [
             "diff": 0,
             "asim": 100,
             "plur": 100,
-            "plzn": 30,
+            "plzn": 20,
             "part": 0
+        }
+    },
+    {
+        "name": "Nativist",
+        "stats": {
+            "bigt": 10,
+            "diff": 50,
+            "asim": 10,
+            "plur": 70,
+            "plzn": 40,
+            "part": 40
+        }
+    },
+    {
+        "name": "Segregationist",
+        "stats": {
+            "bigt": 20,
+            "diff": 50,
+            "asim": 20,
+            "plur": 50,
+            "plzn": 50,
+            "part": 20
+        }
+    },
+    {
+        "name": "Pan-Nationalist",
+        "stats": {
+            "bigt": 0,
+            "diff": 50,
+            "asim": 30,
+            "plur": 100,
+            "plzn": 40,
+            "part": 10
+        }
+    },
+    {
+        "name": "Paleo-Libertarian",
+        "stats": {
+            "bigt": 10,
+            "diff": 70,
+            "asim": 70,
+            "plur": 100,
+            "plzn": 0,
+            "part": 0
+        }
+    },
+    {
+        "name": "Liberal Nationalist",
+        "stats": {
+            "bigt": 0,
+            "diff": 40,
+            "asim": 60,
+            "plur": 60,
+            "plzn": 30,
+            "part": 10
+        }
+    },
+    {
+        "name": "Eco-Fascist",
+        "stats": {
+            "bigt": 50,
+            "diff": 70,
+            "asim": 20,
+            "plur": 0,
+            "plzn": 90,
+            "part": 90
+        }
+    },
+    {
+        "name": "Classical Fascist",
+        "stats": {
+            "bigt": 20,
+            "diff": 50,
+            "asim": 70,
+            "plur": 40,
+            "plzn": 60,
+            "part": 90
+        }
+    },
+    {
+        "name": "Civilizing Colonialist",
+        "stats": {
+            "bigt": 10,
+            "diff": 100,
+            "asim": 10,
+            "plur": 40,
+            "plzn": 90,
+            "part": 20
+        }
+    },
+    {
+        "name": "Liberal Conservative",
+        "stats": {
+            "bigt": 0,
+            "diff": 40,
+            "asim": 80,
+            "plur": 70,
+            "plzn": 20,
+            "part": 0
+        }
+    },
+    {
+        "name": "Right-Wing Populist",
+        "stats": {
+            "bigt": 60,
+            "diff": 60,
+            "asim": 60,
+            "plur": 40,
+            "plzn": 50,
+            "part": 30
         }
     }
 ];
