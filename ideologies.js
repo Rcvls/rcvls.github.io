04dd2bb1b409cc75c324274e7cc7bc77f1d2f6e0
Rcvls <254138130+Rcvls@users.noreply.github.com>
2026-01-11 11:42:22 +0100

    Ideology rebalancing

--- ideologies.js
+++ ideologies.js
@@ -2,7 +2,7 @@ ideologies = [
     {
         "name": "Hitlerite",
         "stats": {
-            "bigt": 80,
+            "bigt": 90,
             "diff": 100,
             "asim": 20,
             "plur": 0,
@@ -15,10 +15,10 @@ ideologies = [
         "stats": {
             "bigt": 15,
             "diff": 80,
-            "asim": 30,
+            "asim": 20,
             "plur": 40,
-            "plzn": 70,
-            "part": 30
+            "plzn": 80,
+            "part": 35
         }
     },
     {
@@ -26,10 +26,10 @@ ideologies = [
         "stats": {
             "bigt": 25,
             "diff": 90,
-            "asim": 20,
+            "asim": 10,
             "plur": 30,
-            "plzn": 80,
-            "part": 40
+            "plzn": 90,
+            "part": 45
         }
     },
     {
@@ -46,64 +46,64 @@ ideologies = [
     {
         "name": "Nativist",
         "stats": {
-            "bigt": 10,
-            "diff": 50,
+            "bigt": 20,
+            "diff": 70,
             "asim": 10,
             "plur": 70,
-            "plzn": 40,
+            "plzn": 60,
             "part": 40
         }
     },
     {
         "name": "Segregationist",
         "stats": {
-            "bigt": 20,
-            "diff": 50,
+            "bigt": 25,
+            "diff": 70,
             "asim": 20,
             "plur": 50,
-            "plzn": 50,
-            "part": 20
+            "plzn": 70,
+            "part": 50
         }
     },
     {
         "name": "Pan-Nationalist",
         "stats": {
-            "bigt": 0,
+            "bigt": 15,
             "diff": 50,
             "asim": 30,
             "plur": 100,
-            "plzn": 40,
-            "part": 10
+            "plzn": 60,
+            "part": 20
         }
     },
     {
         "name": "Paleo-Libertarian",
         "stats": {
-            "bigt": 10,
+            "bigt": 25,
             "diff": 70,
-            "asim": 70,
+            "asim": 60,
             "plur": 100,
-            "plzn": 0,
+            "plzn": 10,
             "part": 0
         }
     },
     {
-        "name": "Liberal Nationalist",
+        "name": "Nationalist",
         "stats": {
-            "bigt": 0,
-            "diff": 40,
+            "bigt": 25,
+            "diff": 50,
             "asim": 60,
             "plur": 60,
-            "plzn": 30,
-            "part": 10
+            "plzn": 50,
+            "part": 40
         }
     },
     {
         "name": "Eco-Fascist",
         "stats": {
-            "bigt": 50,
+            "bigt": 70,
             "diff": 70,
-            "asim": 20,
+            "asim": 10,
             "plur": 0,
             "plzn": 90,
             "part": 90
@@ -112,34 +112,34 @@ ideologies = [
     {
         "name": "Classical Fascist",
         "stats": {
-            "bigt": 20,
+            "bigt": 30,
             "diff": 50,
-            "asim": 70,
-            "plur": 40,
-            "plzn": 60,
+            "asim": 60,
+            "plur": 35,
+            "plzn": 70,
             "part": 90
         }
     },
     {
         "name": "Civilizing Colonialist",
         "stats": {
-            "bigt": 10,
+            "bigt": 40,
             "diff": 100,
             "asim": 10,
             "plur": 40,
             "plzn": 90,
-            "part": 20
+            "part": 25
         }
     },
     {
         "name": "Liberal Conservative",
         "stats": {
-            "bigt": 0,
+            "bigt": 20,
             "diff": 40,
             "asim": 80,
-            "plur": 70,
-            "plzn": 20,
-            "part": 0
+            "plur": 50,
+            "plzn": 30,
+            "part": 15
         }
     },
     {
@@ -158,9 +158,9 @@ ideologies = [
         "stats": {
             "bigt": 20,
             "diff": 50,
-            "asim": 10,
+            "asim": 0,
             "plur": 50,
-            "plzn": 10,
+            "plzn": 50,
             "part": 90
         }
     }
